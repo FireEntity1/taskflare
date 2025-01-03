5e1c9d502d0e7bb4ab6b2e5e29da04fa7875f87f
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-01-03 14:50:57 -0700

    Basic setup

---
+++ taskflare/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+    let projects = $state(
+      [
+        {
+          "name": " ",
+          "description": " ",
+          "tasks": [
+            {
+              "name": " ",
+              "description": " ",
+              "completed": false,
+            }
+          ]
+        }
+      ]
+    )
+    function logData() {
+      console.log(projects);
+    }
+
+    function newTask() {
+      null;
+    }
+</script>
+
+<main>
+  <h1>Taskflare</h1>
+  <button onclick={logData}>Print Data</button>
+</main>
+
+<style>
+  
+</style>
