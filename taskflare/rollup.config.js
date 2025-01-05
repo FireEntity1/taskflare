cadc9430803768d2e19130b723287f6a7451d28e
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-01-05 13:16:26 -0700

    fix: vercel deployment

--- taskflare/rollup.config.js
+++ taskflare/rollup.config.js
@@ -1,19 +1,40 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import css from 'rollup-plugin-css-only';
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: 'src/main.js',
   output: {
-    file: 'public/build/bundle.js',
+    sourcemap: true,
     format: 'iife',
-    name: 'app'
+    name: 'app',
+    file: 'public/build/bundle.js'
   },
   plugins: [
-    svelte(),
-    resolve(),
+    svelte({
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production
+      }
+    }),
+    // Extract CSS into a separate file
+    css({
+      output: 'bundle.css'
+    }),
+    // Resolve node_modules
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
     commonjs(),
-    terser()
-  ]
+    // Minify in production
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
