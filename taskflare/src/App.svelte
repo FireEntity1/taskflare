622f851229c20fcbde5b0af88d77ff9d69bf3c89
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-01-16 13:26:37 -0700

    Fix: everything being completely broken !!

--- taskflare/src/App.svelte
+++ taskflare/src/App.svelte
@@ -1,61 +1,65 @@
 <script>
-    let projects = $state(
-      [
+import "../src/app.css";
+const defaultTask = {
+  name: "New task",
+  description: " ",
+  completed: false,
+}
+
+let data = $state({
+  projects: [
+    {
+      name: "New Project",
+      id: 0,
+      description: "My description",
+      tasks: [
         {
-          "name": "New Project",
-          "id": 0,
-          "description": "My description",
-          "tasks": [
-            {
-              "name": "New task",
-              "description": " ",
-              "completed": false,
-            }
-          ]
+          name: "New task",
+          description: " ",
+          completed: false,
         }
       ]
-    )
-
-    function newTask(id) {
-      projects[id].tasks.push({
-        "name": "New task",
-        "description": " ",
-        "completed": false,
-      })
     }
+  ],
+  highestID: 0
+})
 
-    function newProject() {
-      projects.push({
-        "name": "New Project",
-          "id": 0,
-          "description": "My description",
-          "tasks": [
-            {
-              "name": "New task",
-              "description": " ",
-              "completed": false,
-            }
-          ]
-      })
-    }
+function newTask(id) {
+  data.projects[id].tasks.push({...defaultTask});
+}
+
+function newProject() {
+  data.highestID += 1;
+  data.projects.push({
+    name: "New Project",
+    id: data.highestID,
+    description: "My description",
+    tasks: [
+      {...defaultTask}
+    ]
+  });
+}
 </script>
 
 <main>
   <h1>Taskflare</h1>
-  {#each projects as project}
-  <hr>
+  {#each data.projects as project}
+    <hr>
     <input bind:value={project.name} class="title"> <br>
     <textarea bind:value={project.description} class="description"></textarea>
-
-    {#each project.tasks as task} 
-      <br><label><input type="checkbox" bind:checked={task.completed}> <input bind:value={task.name} class="taskName"> </label>
+    {#each project.tasks as task}
+      <br>
+      <label>
+        <input type="checkbox" bind:checked={task.completed}>
+        <input bind:value={task.name} class="taskName">
+      </label>
     {/each}
-    <br> <button onclick={() => newTask(project.id)}>+</button>
+    <br>
+    <button onclick={() => newTask(project.id)}> + </button>
   {/each}
   <hr>
   <button onclick={newProject}>+</button>
 </main>
 
 <style>
-  
 </style>
---
+++ taskflare/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+	import App from '../App.svelte';
+	let { children } = $props();
+</script>
+
+<App />
+
+{@render children()}
